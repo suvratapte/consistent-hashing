0a8f6965edb2985beca472b8c55379ae64a0e887
Suvrat Apte <suvratapte@Suvrats-MacBook-Air.local>
2020-12-24 22:16:35 +0530

    Make the API better
    Have separate functions for:
    - Creating the ring
    - Adding a node
    - Removing a node
    - Getting corresponding node for a key

--- src/consistent_hashing/core.clj
+++ src/consistent_hashing/core.clj
@@ -47,6 +47,65 @@
             objects)))
 
 
+(defn create-ring
+  [nodes & [hash-fn]]
+  (let [hash-fn (or hash-fn hash)
+        nodes (set nodes)
+        node-hashes (->> nodes
+                         (map hash-fn)
+                         sort)
+        hash->node (reduce (fn [acc node]
+                             (assoc acc (hash node) node))
+                           {}
+                           nodes)]
+    {:node-hashes node-hashes
+     :hash->node hash->node}))
+
+
+(defn add-node
+  [ring-state node & [hash-fn]]
+  (let [hash-fn (or hash-fn hash)
+        current-nodes (-> ring-state :hash->node vals)
+        new-nodes (-> current-nodes (conj node) set)
+        node-hashes (->> new-nodes
+                         (map hash-fn)
+                         sort)
+        hash->node (reduce (fn [acc node]
+                             (assoc acc (hash node) node))
+                           {}
+                           new-nodes)]
+    {:node-hashes node-hashes
+     :hash->node hash->node}))
+
+
+(defn remove-node
+  [ring-state node & [hash-fn]]
+  (let [hash-fn (or hash-fn hash)
+        current-nodes (-> ring-state :hash->node vals set)
+        new-nodes (disj current-nodes node)
+        node-hashes (->> new-nodes
+                         (map hash-fn)
+                         sort)
+        hash->node (reduce (fn [acc node]
+                             (assoc acc (hash node) node))
+                           {}
+                           new-nodes)]
+    {:node-hashes node-hashes
+     :hash->node hash->node}))
+
+
+(defn get-node
+  [ring-state k & [hash-fn]]
+  (let [hash-fn (or hash-fn hash)
+        node-hashes (:node-hashes ring-state)
+        key-hash (hash-fn k)
+        closest-hash (or (->> node-hashes
+                              (take-while #(< % key-hash))
+                              last)
+                         (last node-hashes))]
+    (get (:hash->node ring-state) closest-hash)))
+
+
 (defn get-consistent-hashes
   [nodes objects & [hash-fn]]
   (let [hash-fn (or hash-fn generate-hash)

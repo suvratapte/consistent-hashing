8705088e558668d2833bbb823fdcfd6eb92dff0f
Suvrat Apte <suvratapte@Suvrats-MacBook-Air.local>
2020-12-27 11:50:29 +0530

    Re-arrange code and use public API for demo

--- src/consistent_hashing/core.clj
+++ src/consistent_hashing/core.clj
@@ -3,59 +3,17 @@
   (:require [clojure.pprint :as pp]
             [clojure.string :as cs]))
 
-
-(def objects ["A" "B" "C" "D" "E" "F" "G"
-              "H" "I" "J" "K" "L" "M" "N"
-              "O" "P" "Q" "R" "S" "T" "U"
-              "V" "W" "X" "Y" "Z"])
-
-(def objects-small (take 15 objects))
-
-
-(def cache-nodes [{:name "node-0"
-                   :host "localhost"
-                   :port 6000}
-
-                  {:name "node-1"
-                   :host "localhost"
-                   :port 6001}
-
-                  {:name "node-2"
-                   :host "localhost"
-                   :port 6002}])
-
-
-(defn generate-hash
-  "Generates hash of `object`."
-  [object]
-  (hash object))
-
-
-(defn get-mod-n-hashes
-  "Returns mod `n` hashes for `objects`.
-  Optionally, a custom hash function can be provided as last argument (`hash-fn`).
-  This funtion should accept a single argument and return hash of that argument."
-  [n objects & [hash-fn]]
-  (let [hash-fn (or hash-fn generate-hash)]
-    (reduce (fn [acc object]
-              (let [object-hash (-> object hash-fn (mod n))]
-                (update acc
-                        object-hash
-                        (fnil conj [])
-                        object)))
-            {}
-            objects)))
-
+;; ────────────────────────────────── Public API ──────────────────────────────────
 
 (defn create-ring
   [nodes & [hash-fn]]
   (let [hash-fn (or hash-fn hash)
         nodes (set nodes)
         node-hashes (->> nodes
-                         (map hash-fn)
+                         (map #(-> % :name hash-fn))
                          sort)
         hash->node (reduce (fn [acc node]
-                             (assoc acc (hash node) node))
+                             (assoc acc (-> node :name hash-fn) node))
                            {}
                            nodes)]
     {:node-hashes node-hashes
@@ -105,35 +63,60 @@
                          (last node-hashes))]
     (get (:hash->node ring-state) closest-hash)))
 
+;; ────────────────────────────────────────────────────────────────────────────────
 
-(defn get-consistent-hashes
-  [nodes objects & [hash-fn]]
-  (let [hash-fn (or hash-fn generate-hash)
-        server-hashes (-> #(hash-fn (:name %))
-                          (map nodes)
-                          sort)
-        hash->node (reduce (fn [acc node]
-                             (assoc acc
-                                    (hash (:name node))
-                                    (:name node)))
-                           {}
-                           nodes)]
+
+;; ───────────────────────────────────── Usage ────────────────────────────────────
+
+(def objects ["A" "B" "C" "D" "E" "F" "G"
+              "H" "I" "J" "K" "L" "M" "N"
+              "O" "P" "Q" "R" "S" "T" "U"
+              "V" "W" "X" "Y" "Z"])
+
+(def objects-small (take 15 objects))
+
+
+(def cache-nodes [{:name "node-0"
+                   :host "localhost"
+                   :port 6000}
+
+                  {:name "node-1"
+                   :host "localhost"
+                   :port 6001}
+
+                  {:name "node-2"
+                   :host "localhost"
+                   :port 6002}])
+
+
+(defn get-mod-n-hashes
+  "Returns mod `n` hashes for `objects`.
+  Optionally, a custom hash function can be provided as last argument (`hash-fn`).
+  This funtion should accept a single argument and return hash of that argument."
+  [n objects & [hash-fn]]
+  (let [hash-fn (or hash-fn generate-hash)]
     (reduce (fn [acc object]
-              (let [object-hash (hash-fn object)
-                    closest-server-hash (or (-> #(< % object-hash)
-                                                (take-while server-hashes)
-                                                last)
-                                            (last server-hashes))
-                    closest-server-node (hash->node closest-server-hash)]
+              (let [object-hash (-> object hash-fn (mod n))]
                 (update acc
-                        closest-server-node
+                        object-hash
                         (fnil conj [])
                         object)))
             {}
             objects)))
 
 
-(def mod-hashes (get-mod-n-hashes (count cache-nodes) objects))
+(defn get-consistent-hashes
+  [nodes objects & [hash-fn]]
+  (let [hash-fn (or hash-fn hash)
+        ring-state (create-ring nodes hash-fn)]
+    (reduce (fn [acc object]
+              (let [node (get-node ring-state object hash-fn)]
+                (update acc
+                        (:name node)
+                        (fnil conj [])
+                        object)))
+            {}
+            objects)))
 
 
 (let [separator (->> "─" (repeat 60) cs/join)]
